f09c78822d9ad037f0d1e293b2a14e1be66f6d13
Shivansh Singhal <shivansh@pop-os.localdomain>
2022-01-27 20:58:21 +0530

    Converted function to class

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-function todceno(options) {
-    let images = document.querySelectorAll('.tod-ceno');
+// function todceno(options) {
+//     let images = document.querySelectorAll('.tod-ceno');
 
-    if(options.shadow === 'hard')
-        options.shadow = '0px'
-    else
-        options.shadow = '10px'
+//     if(options.shadow === 'hard')
+//         options.shadow = '0px'
+//     else
+//         options.shadow = '10px'
     
-    images.forEach(image => {
-        image.style.boxShadow = `7px 7px ${options.shadow} 1px rgba(0,0,0,0.12)`;
+//     images.forEach(image => {
+//         image.style.boxShadow = `7px 7px ${options.shadow} 1px rgba(0,0,0,0.12)`;
 
-        if(options.padding) {
-            options.padding = '2em';
-        }
-    })
+//         if(options.padding) {
+//             options.padding = '2em';
+//         }
+//     })
+// }
+
+class todceno {
+    constructor(shadow, padding) {
+        this.shadow = shadow;
+        this.padding = padding;
+    }
+
+    giveShadow() {
+        let images = document.querySelectorAll('.tod-ceno');
+        
+        images.forEach(image => {
+            image.style.boxShadow = `7px 7px ${this.shadow} 1px rgba(0,0,0,0.12)`;
+        })
+    }
 }
 
 module.exports.todceno = todceno;
