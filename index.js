3ad8b1512ca1dba926185d8031ee54c57107cc2a
Shivansh Singhal <shivansh@pop-os.localdomain>
2022-01-27 21:12:35 +0530

    Added console logs

--- index.js
+++ index.js
@@ -19,10 +19,13 @@ class todceno {
     constructor(shadow, padding) {
         this.shadow = shadow;
         this.padding = padding;
+
+        console.log(shadow, padding);
     }
 
     giveShadow() {
         let images = document.querySelectorAll('.tod-ceno');
+        console.log(images.length)
         
         images.forEach(image => {
             image.style.boxShadow = `7px 7px ${this.shadow} 1px rgba(0,0,0,0.12)`;
